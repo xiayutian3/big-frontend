8fbc0a1759f9466ebdfdc9c7af1689a28d0694bd
1127071993@qq.com <aini2009626>
2020-05-10 22:01:47 +0800

    （JWT登录鉴权—通用登录模块后端开发）5-2 注册接口Mock数据

--- 9.JWT登录鉴权—通用登录模块后端开发/front-start/src/config/index.js
+++ 9.JWT登录鉴权—通用登录模块后端开发/front-start/src/config/index.js
@@ -1,6 +1,9 @@
 export default {
   baseUrl: {
-    dev: 'http://localhost:3000',
+    // doclever mock server
+    dev: 'http://localhost:36742',
+    // 我们搭建的nodejs服务
+    // dev: 'http://localhost:3000',
     pro: 'http://www.toimc.com:12000'
   }
 }
--- 9.JWT登录鉴权—通用登录模块后端开发/front-start/src/views/Reg.vue
+++ 9.JWT登录鉴权—通用登录模块后端开发/front-start/src/views/Reg.vue
@@ -14,7 +14,7 @@
               <validation-observer ref="observer">
                 <form method="post">
                   <div class="layui-form-item">
-                    <validation-provider name="email" rules="required|email" v-slot="{errors}">
+                    <validation-provider name="username" rules="required|email" v-slot="{errors}">
                       <div class="layui-row">
                         <label for="L_email" class="layui-form-label">用户名</label>
                         <div class="layui-input-inline">
