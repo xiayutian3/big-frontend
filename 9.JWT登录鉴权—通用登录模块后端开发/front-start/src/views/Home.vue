a36bbb12074d3e56350b8d020ffeca29641a2c27
1127071993@qq.com <aini2009626>
2020-05-15 22:02:29 +0800

    （全栈开发—首页模块）4-4 初始识响应式原理

---
+++ 9.JWT登录鉴权—通用登录模块后端开发/front-start/src/views/Home.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="home">
+    <div class="panel">
+      <div class="layui-container">
+        <ul>
+          <li class="layui-hide-xs"><a href="">首页</a></li>
+          <li><a href="">提问</a></li>
+          <li><a href="">分享</a></li>
+          <li><a href="">讨论</a></li>
+          <li><a href="">建议</a></li>
+          <li><a href="">公告</a></li>
+          <li><a href="">动态</a></li>
+          <li class="layui-hide-xs">
+            <span class="line"></span>
+          </li>
+          <li class="layui-hide-xs layui-hide-sm"><a href="">我发表的贴</a></li>
+          <li class="layui-hide-xs layui-hide-sm"><a href="">我收藏的贴</a></li>
+        </ul>
+        <div class="right layui-hide-xs">
+          <span class="layui-icon layui-icon-search"></span>
+          <a href="" class="layui-btn">发表新帖</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  props: {},
+  data () {
+    return {
+    }
+  },
+  created () {},
+  mounted () {},
+  computed: {},
+  methods: {},
+  components: {},
+  watch: {}
+}
+</script>
+<style lang="scss">
+@media (max-width:768px) {
+  .panel{
+    height: auto !important;
+  }
+  ul{
+    li{
+      width:33.3%
+    }
+  }
+}
+.home{
+  .panel{
+    position: relative;
+    height: 50px;
+    line-height: 50px;
+    margin-bottom: 15px;
+    border-radius: 2px;
+    background-color: #fff;
+    box-shadow: 0 1px 2px 0 rgba(0,0,0,.05);
+    ul{
+      li{
+        display: inline-block;
+        a{
+          padding: 0 20px;
+        }
+      }
+      .line{
+        display: inline-block;
+        height: 10px;
+        width: 1px;
+        background-color: #e2e2e2;
+        vertical-align: middle;
+        margin: 0 20px;
+      }
+    }
+    .right{
+      position: absolute;
+      right: 0;
+      top: 0;
+      .layui-icon-search{
+        margin-right: 10px;
+      }
+    }
+  }
+}
+</style>
