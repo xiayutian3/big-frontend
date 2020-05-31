38362a6ff9f4a4cccd0a1a527abfead64018bced
1127071993@qq.com <aini2009626>
2020-05-31 12:40:16 +0800

    3-5 用户签到逻辑介绍

--- 9.JWT登录鉴权—通用登录模块后端开发/front-start/src/components/sidebar/Sign.vue
+++ 9.JWT登录鉴权—通用登录模块后端开发/front-start/src/components/sidebar/Sign.vue
@@ -27,93 +27,14 @@
           <span>获得了<cite>20</cite>飞吻</span>
       -->
     </div>
-    <div class="modal" v-show="isShow">
-      <div class="mask" @click="close"></div>
-      <div class="layui-layer layui-layer-page info" :class="{active:isShow}">
-        <div class="layui-layer-title">
-          签到说明
-          <i class="layui-icon layui-icon-close pull-right" @click="close"></i>
-        </div>
-        <div class="layui-layer-content">
-          <div class="layui-text">
-            <blockquote class="layui-elem-quote">“签到”可获得的社区积分，规则如下</blockquote>
-            <table class="layui-table">
-              <thead>
-                <tr>
-                  <th>连续签到天数</th>
-                  <th>每天可获积分</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>&lt;5</td>
-                  <td>5</td>
-                </tr>
-                <tr>
-                  <td>&ge;5</td>
-                  <td>10</td>
-                </tr>
-                <tr>
-                  <td>&ge;15</td>
-                  <td>15</td>
-                </tr>
-                <tr>
-                  <td>&ge;30</td>
-                  <td>20</td>
-                </tr>
-                <tr>
-                  <td>&ge;100</td>
-                  <td>30</td>
-                </tr>
-                <tr>
-                  <td>&ge;365</td>
-                  <td>50</td>
-                </tr>
-              </tbody>
-            </table>
-            <div>
-              <p>中间若有间隔，则连续天数重新计算</p>
-              <p class="orange">不可复用程序自动签到，否则积分清零</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="modal" v-show="showList">
-      <div class="mask" @click="close"></div>
-      <div class="layui-layer layui-layer-page info" :class="{active:isShow}">
-        <div class="layui-layer-title">
-          签到活跃榜 - TOP
-          <i class="layui-icon layui-icon-close pull-right" @click="close"></i>
-        </div>
-        <div class="layui-layer-content pd0">
-          <div class="layui-tab layui-tab-brief">
-            <ul class="layui-tab-title">
-              <li :class="{'layui-this':current===0}" @click="choose(0)">最新签到</li>
-              <li :class="{'layui-this':current===1}" @click="choose(1)">今日热议</li>
-              <li :class="{'layui-this':current===2}" @click="choose(2)">总签到榜</li>
-            </ul>
-            <div class="layui-tab-content">
-              <ul class="layui-tab-item layui-show">
-                <li v-for="(item,index) in lists" :key="'sign'+index">
-                  <img src="/img/bear.jpg" alt class="mr1" />
-                  <cite class="fly-link">{{item.name}}</cite>
-                  <span class="fly-grey" v-if="current !== 2">签到于 {{item.created}}</span>
-                  <span class="fly-grey" v-else>
-                    已经连续签到
-                    <i class="orange">{{item.count}}</i>天
-                  </span>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <SignInfo :isShow="isShow" @closeModal="close"/>
+    <SignList :isShow="showList" @closeModal="close"/>
   </div>
 </template>
 
 <script>
+import SignInfo from './SignInfo'
+import SignList from './SignList'
 export default {
   name: 'sign',
   props: {},
@@ -121,29 +42,7 @@ export default {
     return {
       isShow: false,
       showList: false,
-      current: 0,
-      lists: [
-        {
-          name: 'test1',
-          count: 4,
-          created: '2020-5-30'
-        },
-        {
-          name: 'test1',
-          count: 4,
-          created: '2020-5-30'
-        },
-        {
-          name: 'test1',
-          count: 4,
-          created: '2020-5-30'
-        },
-        {
-          name: 'test1',
-          count: 4,
-          created: '2020-5-30'
-        }
-      ]
+      current: 0
     }
   },
   created () {},
@@ -164,11 +63,14 @@ export default {
       this.current = num
     }
   },
-  components: {},
+  components: {
+    SignInfo,
+    SignList
+  },
   watch: {}
 }
 </script>
-<style lang="scss" scoped>
+<style lang="scss">
 @keyframes bounceIn {
   0% {
     opacity: 0;
