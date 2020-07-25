1110f682711e9eca506f05772cac4f7e886e293a
1127071993@qq.com <aini2009626>
2020-07-25 16:07:28 +0800

    delete nuxted git config

---
+++ 9.JWT登录鉴权—通用登录模块后端开发/nuxted/.nuxt/components/index.js
@@ -0,0 +1,75 @@
+export { default as Footer } from '../..\\components\\Footer.vue'
+export { default as Header } from '../..\\components\\Header.vue'
+export { default as Panel } from '../..\\components\\Panel.vue'
+export { default as Add } from '../..\\components\\contents\\Add.vue'
+export { default as Detail } from '../..\\components\\contents\\Detail.vue'
+export { default as Edit } from '../..\\components\\contents\\Edit.vue'
+export { default as ListItem } from '../..\\components\\contents\\ListItem.vue'
+export { default as Top } from '../..\\components\\contents\\Top.vue'
+export { default as Ads } from '../..\\components\\sidebar\\Ads.vue'
+export { default as HotList } from '../..\\components\\sidebar\\HotList.vue'
+export { default as Links } from '../..\\components\\sidebar\\Links.vue'
+export { default as Sign } from '../..\\components\\sidebar\\Sign.vue'
+export { default as SignInfo } from '../..\\components\\sidebar\\SignInfo.vue'
+export { default as SignList } from '../..\\components\\sidebar\\SignList.vue'
+export { default as Tips } from '../..\\components\\sidebar\\Tips.vue'
+export { default as Center } from '../..\\components\\user\\Center.vue'
+export { default as Msg } from '../..\\components\\user\\Msg.vue'
+export { default as Others } from '../..\\components\\user\\Others.vue'
+export { default as Posts } from '../..\\components\\user\\Posts.vue'
+export { default as Setting } from '../..\\components\\user\\Setting.vue'
+export { default as Alert } from '../..\\components\\modules\\alert\\Alert.vue'
+export { default as Alert } from '../..\\components\\modules\\alert\\index.js'
+export { default as Emwdaima } from '../..\\components\\modules\\editor\\Emwdaima.vue'
+export { default as Face } from '../..\\components\\modules\\editor\\Face.vue'
+export { default as ImgUpload } from '../..\\components\\modules\\editor\\ImgUpload.vue'
+export { default as Index } from '../..\\components\\modules\\editor\\Index.vue'
+export { default as LinkAdd } from '../..\\components\\modules\\editor\\LinkAdd.vue'
+export { default as Preview } from '../..\\components\\modules\\editor\\Preview.vue'
+export { default as Quote } from '../..\\components\\modules\\editor\\Quote.vue'
+export { default as Pop } from '../..\\components\\modules\\pop\\index.js'
+export { default as Pop } from '../..\\components\\modules\\pop\\Pop.vue'
+export { default as Accounts } from '../..\\components\\user\\common\\Accounts.vue'
+export { default as MyCollection } from '../..\\components\\user\\common\\MyCollection.vue'
+export { default as MyInfo } from '../..\\components\\user\\common\\MyInfo.vue'
+export { default as MyPost } from '../..\\components\\user\\common\\MyPost.vue'
+export { default as Passwd } from '../..\\components\\user\\common\\Passwd.vue'
+export { default as PicUpload } from '../..\\components\\user\\common\\PicUpload.vue'
+
+export const LazyFooter = import('../..\\components\\Footer.vue' /* webpackChunkName: "components_Footer'}" */).then(c => c.default || c)
+export const LazyHeader = import('../..\\components\\Header.vue' /* webpackChunkName: "components_Header'}" */).then(c => c.default || c)
+export const LazyPanel = import('../..\\components\\Panel.vue' /* webpackChunkName: "components_Panel'}" */).then(c => c.default || c)
+export const LazyAdd = import('../..\\components\\contents\\Add.vue' /* webpackChunkName: "components_contents/Add'}" */).then(c => c.default || c)
+export const LazyDetail = import('../..\\components\\contents\\Detail.vue' /* webpackChunkName: "components_contents/Detail'}" */).then(c => c.default || c)
+export const LazyEdit = import('../..\\components\\contents\\Edit.vue' /* webpackChunkName: "components_contents/Edit'}" */).then(c => c.default || c)
+export const LazyListItem = import('../..\\components\\contents\\ListItem.vue' /* webpackChunkName: "components_contents/ListItem'}" */).then(c => c.default || c)
+export const LazyTop = import('../..\\components\\contents\\Top.vue' /* webpackChunkName: "components_contents/Top'}" */).then(c => c.default || c)
+export const LazyAds = import('../..\\components\\sidebar\\Ads.vue' /* webpackChunkName: "components_sidebar/Ads'}" */).then(c => c.default || c)
+export const LazyHotList = import('../..\\components\\sidebar\\HotList.vue' /* webpackChunkName: "components_sidebar/HotList'}" */).then(c => c.default || c)
+export const LazyLinks = import('../..\\components\\sidebar\\Links.vue' /* webpackChunkName: "components_sidebar/Links'}" */).then(c => c.default || c)
+export const LazySign = import('../..\\components\\sidebar\\Sign.vue' /* webpackChunkName: "components_sidebar/Sign'}" */).then(c => c.default || c)
+export const LazySignInfo = import('../..\\components\\sidebar\\SignInfo.vue' /* webpackChunkName: "components_sidebar/SignInfo'}" */).then(c => c.default || c)
+export const LazySignList = import('../..\\components\\sidebar\\SignList.vue' /* webpackChunkName: "components_sidebar/SignList'}" */).then(c => c.default || c)
+export const LazyTips = import('../..\\components\\sidebar\\Tips.vue' /* webpackChunkName: "components_sidebar/Tips'}" */).then(c => c.default || c)
+export const LazyCenter = import('../..\\components\\user\\Center.vue' /* webpackChunkName: "components_user/Center'}" */).then(c => c.default || c)
+export const LazyMsg = import('../..\\components\\user\\Msg.vue' /* webpackChunkName: "components_user/Msg'}" */).then(c => c.default || c)
+export const LazyOthers = import('../..\\components\\user\\Others.vue' /* webpackChunkName: "components_user/Others'}" */).then(c => c.default || c)
+export const LazyPosts = import('../..\\components\\user\\Posts.vue' /* webpackChunkName: "components_user/Posts'}" */).then(c => c.default || c)
+export const LazySetting = import('../..\\components\\user\\Setting.vue' /* webpackChunkName: "components_user/Setting'}" */).then(c => c.default || c)
+export const LazyAlert = import('../..\\components\\modules\\alert\\Alert.vue' /* webpackChunkName: "components_modules/alert/Alert'}" */).then(c => c.default || c)
+export const LazyAlert = import('../..\\components\\modules\\alert\\index.js' /* webpackChunkName: "components_modules/alert/index'}" */).then(c => c.default || c)
+export const LazyEmwdaima = import('../..\\components\\modules\\editor\\Emwdaima.vue' /* webpackChunkName: "components_modules/editor/Emwdaima'}" */).then(c => c.default || c)
+export const LazyFace = import('../..\\components\\modules\\editor\\Face.vue' /* webpackChunkName: "components_modules/editor/Face'}" */).then(c => c.default || c)
+export const LazyImgUpload = import('../..\\components\\modules\\editor\\ImgUpload.vue' /* webpackChunkName: "components_modules/editor/ImgUpload'}" */).then(c => c.default || c)
+export const LazyIndex = import('../..\\components\\modules\\editor\\Index.vue' /* webpackChunkName: "components_modules/editor/Index'}" */).then(c => c.default || c)
+export const LazyLinkAdd = import('../..\\components\\modules\\editor\\LinkAdd.vue' /* webpackChunkName: "components_modules/editor/LinkAdd'}" */).then(c => c.default || c)
+export const LazyPreview = import('../..\\components\\modules\\editor\\Preview.vue' /* webpackChunkName: "components_modules/editor/Preview'}" */).then(c => c.default || c)
+export const LazyQuote = import('../..\\components\\modules\\editor\\Quote.vue' /* webpackChunkName: "components_modules/editor/Quote'}" */).then(c => c.default || c)
+export const LazyPop = import('../..\\components\\modules\\pop\\index.js' /* webpackChunkName: "components_modules/pop/index'}" */).then(c => c.default || c)
+export const LazyPop = import('../..\\components\\modules\\pop\\Pop.vue' /* webpackChunkName: "components_modules/pop/Pop'}" */).then(c => c.default || c)
+export const LazyAccounts = import('../..\\components\\user\\common\\Accounts.vue' /* webpackChunkName: "components_user/common/Accounts'}" */).then(c => c.default || c)
+export const LazyMyCollection = import('../..\\components\\user\\common\\MyCollection.vue' /* webpackChunkName: "components_user/common/MyCollection'}" */).then(c => c.default || c)
+export const LazyMyInfo = import('../..\\components\\user\\common\\MyInfo.vue' /* webpackChunkName: "components_user/common/MyInfo'}" */).then(c => c.default || c)
+export const LazyMyPost = import('../..\\components\\user\\common\\MyPost.vue' /* webpackChunkName: "components_user/common/MyPost'}" */).then(c => c.default || c)
+export const LazyPasswd = import('../..\\components\\user\\common\\Passwd.vue' /* webpackChunkName: "components_user/common/Passwd'}" */).then(c => c.default || c)
+export const LazyPicUpload = import('../..\\components\\user\\common\\PicUpload.vue' /* webpackChunkName: "components_user/common/PicUpload'}" */).then(c => c.default || c)
